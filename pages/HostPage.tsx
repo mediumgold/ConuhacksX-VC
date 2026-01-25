0a0244c26021adc00e3281385533ad7a26e1a571
mediumgold <nathanchristopher.c@gmail.com>
2026-01-25 09:50:32 -0500

    change home page title

--- pages/HostPage.tsx
+++ pages/HostPage.tsx
@@ -738,7 +738,7 @@ const HostPage: React.FC = () => {
         className={`min-h-screen h-full bg-black text-white overflow-y-auto ${isLobby ? '' : 'hidden'}`}
       >
           <div className="max-w-4xl mx-auto p-4 md:p-8 pb-20">
-            <h1 className="text-2xl md:text-4xl font-black text-cyan-500 mb-2">VOCAL COMBAT</h1>
+            <h1 className="text-2xl md:text-4xl font-black text-cyan-500 mb-2">KARAOKE COMBAT</h1>
             <p className="text-yellow-500 mb-4 md:mb-8 text-sm md:text-base">HOST SETUP</p>
 
           {/* Ngrok Setup Info */}
